e26951366bd08a546cfd47e140a6eaac2247a54f
Erazul Islam <erazul89@gmail.com>
2024-03-02 21:47:22 +0600

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Choose from "@/components/Choose";
+import Trip from "@/components/ui/Trip";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Choose></Choose>
+      <Trip></Trip>
     </main>
   );
 }
---
+++ src/components/ui/Trip.tsx
@@ -0,0 +1,11 @@
+const Trip = () => {
+    return (
+        <div className="mt-8">
+            <p className="text-center">Browse by Category</p>
+            <p className="text-center mt-6 text-3xl font-bold">Find Inspiration For <br />
+                Your Next Trip</p>
+        </div>
+    );
+};
+
+export default Trip;
