c379ace89f8f6517114083fb4e610f06839140b2
mdArifulIslam12 <mdarif767778@gmail.com>
2024-03-05 15:11:11 +0600

    update

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,6 +1,7 @@
 import SheardButtonIcons from "@/components/Sheard/SheardButtonIcons";
 import SheardHeader from "@/components/Sheard/SheardHeader";
 import SheardNewsletterSupport from "@/components/Sheard/SheardNewsletterSupport";
+import AboutEasyBookingDeatils from "@/components/aboutUs/AboutEasyBookingDeatils";
 import TravelTeam from "@/components/aboutUs/TravelTeam";
 import WorldExciting from "@/components/aboutUs/WorldExciting";
 import AboutIconTourDetails from "@/components/aboutUs/aboutIconTourDetails";
@@ -12,9 +13,9 @@ const AboutUs = () => {
       <SheardHeader title={"About Us"} subTitle="about us" />
       <div className="max-w-7xl mx-auto">
         <AboutSubHeader />
-
         <AboutIconTourDetails />
       </div>
+      <AboutEasyBookingDeatils />
 
       <SheardButtonIcons className="w-24 h-24 relative">dsfd</SheardButtonIcons>
 
