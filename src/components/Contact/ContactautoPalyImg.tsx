11fbb4d126245d8a40b1c84ef5ed356e7ed962df
mdArifulIslam12 <mdarif767778@gmail.com>
2024-03-06 16:42:24 +0600

    contact

---
+++ src/components/Contact/ContactautoPalyImg.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+const ContactautoPalyImg = () => {
+  return (
+    <div className="mt-48 mb-24  about-page-auto-paly-img ">
+      <div className="slider">
+        <div className="slide-track">
+          <div className="slide">
+            <img
+              src="https://demo2.wpopal.com/zourney/wp-content/uploads/2022/07/contact_pic8.jpg"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://demo2.wpopal.com/zourney/wp-content/uploads/2022/07/contact_pic8.jpg"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://demo2.wpopal.com/zourney/wp-content/uploads/2022/07/contact_pic8.jpg"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://demo2.wpopal.com/zourney/wp-content/uploads/2022/07/contact_pic8.jpg"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://demo2.wpopal.com/zourney/wp-content/uploads/2022/07/contact_pic8.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactautoPalyImg;
